cdd276d3dc8853d20622e42dbf66ae38d082877f
Kenth Andersson <kenth@eastmark.net>
2024-08-29 16:42:22 +0200

    Fixed issue in large codebases

--- lua/eastmark/lspconfig.lua
+++ lua/eastmark/lspconfig.lua
@@ -3,6 +3,7 @@ local base = require "nvchad.configs.lspconfig"
 local lspconfig = require "lspconfig"
 
 lspconfig.clangd.setup {
+    cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose', '-ferror-limit=0'},
     init_options = {
         fallbackFlags = {'--std=c++11'},
     },
